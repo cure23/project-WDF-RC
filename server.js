f1c2ba923429cc68661af497fcfff06aa2f4d6b9
cure23 <cure23xv@student.ju.se>
2025-09-19 09:27:46 +0200

    projects handelbar - lab3

--- server.js
+++ server.js
@@ -5,6 +5,68 @@ const { engine } = require("express-handlebars");
 const port = 8080;
 const app = express();
 
+// MODEL
+const projects = [
+  {
+    id: "1",
+    me: "Counting people with a camera",
+    type: "Research",
+    type: "Research",
+    desc: "The purpose of this project is to count people passing through a corridor and to know how many are in the room at a certain time.",
+    year: 2022,
+    dev: "Python and OpenCV (Computer vision) library",
+    url: "/img/counting.png",
+  },
+  {
+    id: "2",
+    name: "Visualisation of 3D medical images",
+    type: "Research",
+    desc: "The project makes a 3D model of the analysis of the body of a person and displays the detected health problems. It is useful for doctors to view in 3D their patients and the evolution of a disease.",
+    year: 2012,
+    url: "/img/medical.png",
+  },
+  {
+    id: "3",
+    name: "Multiple questions system",
+    type: "Teaching",
+    des: "During the lockdowns in France, this project was useful to test the students online with a Quizz system.",
+    year: 2021,
+    url: "/img/qcm07.png",
+  },
+  {
+    id: "4",
+    name: "Image comparison with the Local Dissmilarity Map",
+    desc: "The project is about finding and quantifying the differences between two images of the same size. The applications were numerous: satellite imaging, medical imaging,...images of the same size.",
+    year: 2020,
+    type: "Research",
+    url: "/img/diaw02.png",
+  },
+  {
+    id: "5",
+    name: "Management system for students' internships",
+    desc: "This project was about the creation of a database to manage the students' internships.",
+    year: 2012,
+    type: "Teaching",
+    url: "/img/management.png",
+  },
+  {
+    id: "6",
+    name: "Magnetic Resonance Spectroscopy",
+    desc: "Analysis of signals and images from Magnetic Resonance Spectroscopy and Imaging.",
+    year: 2013,
+    type: "Research",
+    url: "/img/yu00.png",
+  },
+  {
+    id: "7",
+    name: "Signal Analysis for Detection of Epileptic Deseases",
+    desc: "This project was about the detection of epileptic problems in signals.",
+    year: 2019,
+    type: "Research",
+    url: "/img/youssef00.png",
+  },
+];
+
 // HANDLEBARS
 app.engine("handlebars", engine()); // initialize the engine to be handlebars
 app.set("view-engine", "handlebars"); // set handlebars as the view engine
@@ -103,6 +165,12 @@ app.get("/about", (req, res) => {
 // DEFINE THE /contact ROUTE (MY Contact)
 app.get("/contact", (req, res) => {
   res.render("contact.handlebars");
+});
+
+// DEFINE THE /projects ROUTE (MY projects)
+app.get("/projects", (req, res) => {
+  model = { projects };
+  res.render("projects.handlebars", model);
 });
 
 app.listen(port, () => {
